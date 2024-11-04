db67be36f525c610cdb9eec286b0f08c41dfee89
Влад Козлов <vkozlov15012005@gmail.com>
2024-11-04 14:49:45 +0300

    Добавьте файлы проекта.

---
+++ AlaniaDrift/App.xaml.cs
@@ -0,0 +1,27 @@
+﻿using AlaniaDrift.Model;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace AlaniaDrift
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private static AlaniaDriftDbEntities _context;
+        public static AlaniaDriftDbEntities GetContext()
+        {
+            if (_context == null)
+            {
+                _context = new AlaniaDriftDbEntities();
+            }
+            return _context;
+        }
+    }
+}
---
+++ AlaniaDrift/Views/Pages/AuthorisationPage.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AlaniaDrift.Views.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для AuthorisationPage.xaml
+    /// </summary>
+    public partial class AuthorisationPage : Page
+    {
+        private static readonly Regex _regex = new Regex("[^0-9.-]+");
+        public AuthorisationPage()
+        {
+            InitializeComponent();
+        }
+
+        private void EntryBTn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void SignUpBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void PhoneNUmberTb_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = !IsTextAllowed(e.Text);
+        }
+        private static bool IsTextAllowed(string text)
+        {
+            return !_regex.IsMatch(text);
+        }
+    }
+}
